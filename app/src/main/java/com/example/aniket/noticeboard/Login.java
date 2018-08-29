377badb26d3d4fee9ce29dac0e79b535b64b1b38
aniket <aniket14goyal@gmail.com>
2018-08-29 12:16:26 +0530

    login working

--- app/src/main/java/com/example/aniket/noticeboard/Login.java
+++ app/src/main/java/com/example/aniket/noticeboard/Login.java
@@ -1,11 +1,17 @@
 package com.example.aniket.noticeboard;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Html;
+import android.text.TextWatcher;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import retrofit2.Call;
@@ -15,17 +21,15 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /*
-To add this to your app :
-- add internet permission to your gradle
-- copy this class in your app
-- copy activity_login.xml to your app
+on successful login saving data in shared preferences in name : "Noticeboard_data"
+with refresh token with key "refresh_token"
+with access token with key "access token"
  */
 
 public class Login extends AppCompatActivity {
-
-    EditText UsernameText ;//= findViewById(R.id.Username);
-    EditText PasswordText ;//= findViewById(R.id.Password);
-    Button SubmitButton ;//= findViewById(R.id.Submit);
+    EditText UsernameText ;
+    EditText PasswordText ;
+    Button SubmitButton ;
     static String base_url="http://127.0.0.1:8000";
     static Retrofit retrofit;
     static api_interface api_service;
@@ -33,23 +37,39 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        findViewById(R.id.clear_focus).requestFocus();
+        TextView link= (TextView) findViewById(R.id.link);
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView img_love= (TextView) findViewById(R.id.made_with_love);
+        String text = "Made with <font color=#F50057>"+String.valueOf(Character.toChars(0x2764))+"</font> by IMG";
+        img_love.setText(Html.fromHtml(text));
         UsernameText=findViewById(R.id.Username);
         PasswordText=findViewById(R.id.Password);
         SubmitButton=findViewById(R.id.Submit);
         SubmitButton.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //make api call
-                //save token in shared preferences
-                //on fail of login show login  fail
-                //check for internet connection (on fail of api call either internet not available or server down)
                 api_service = getRetrofitInstance().create(api_interface.class);
                 login_credentials credentials=new login_credentials(UsernameText.getText().toString(),PasswordText.getText().toString());
                 Call<login_response> call= api_service.login(credentials);
                 call.enqueue(new Callback<login_response>() {
                     @Override
                     public void onResponse(Call<login_response> call, Response<login_response> response) {
-                        Toast.makeText(Login.this, "success  "+response, Toast.LENGTH_SHORT).show();
+                        if(response.body()==null)
+                        {
+                            Toast.makeText(Login.this, "wrong credentials :(", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            SharedPreferences pref= getApplicationContext().getSharedPreferences("Noticeboard_data",0);
+                            SharedPreferences.Editor edit=pref.edit();
+                            edit.putString("refresh_token",response.body().getRefresh());
+                            edit.putString("access_token",response.body().getAccess());
+                            edit.commit();
+
+                            // new screen intent .......
+
+                        }
                     }
 
                     @Override
@@ -57,6 +77,50 @@ public class Login extends AppCompatActivity {
                         Toast.makeText(Login.this, "connection issue", Toast.LENGTH_SHORT).show();
                     }
                 });
+            }
+        });
+        UsernameText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!UsernameText.getText().toString().trim().isEmpty() && !PasswordText.getText().toString().trim().isEmpty()) {
+                    SubmitButton.setEnabled(true);
+
+                } else {
+                    SubmitButton.setEnabled(false);
+                }
+
+            }
+        });
+        PasswordText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!UsernameText.getText().toString().trim().isEmpty() && !PasswordText.getText().toString().trim().isEmpty()) {
+                    SubmitButton.setEnabled(true);
+
+                } else {
+                    SubmitButton.setEnabled(false);
+                }
+
             }
         });
     }
