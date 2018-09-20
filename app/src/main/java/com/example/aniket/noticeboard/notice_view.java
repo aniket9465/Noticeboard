fb3cc5e1df2e114b2d7b04a8610f0cc14b554287
aniket <aniket14goyal@gmail.com>
2018-09-20 12:25:30 +0530

    started notice view

--- app/src/main/java/com/example/aniket/noticeboard/notice_view.java
+++ app/src/main/java/com/example/aniket/noticeboard/notice_view.java
@@ -3,6 +3,11 @@ package com.example.aniket.noticeboard;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class notice_view extends AppCompatActivity
 {
@@ -10,5 +15,26 @@ public class notice_view extends AppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notice_view);
+        WebView browser = (WebView) findViewById(R.id.webview);
+        browser.getSettings().setBuiltInZoomControls(true);
+        browser.getSettings().setDisplayZoomControls(false);
+        browser.setWebViewClient(new WebViewClient()
+        {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        browser.loadUrl("http://www.google.com");
+
+        ((ImageView)(findViewById(R.id.back_button))).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
+
     }
 }
